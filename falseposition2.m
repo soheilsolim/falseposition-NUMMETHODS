6b0f269ee8cdd10ce16c841f8c572ebe5ad7de9b
soheil <140661028+soheilsolim@users.noreply.github.com>
2024-03-14 11:15:23 -0700

    Add files via upload

---
+++ falseposition2.m
@@ -0,0 +1,31 @@
+% soheilsolim
+clc;
+clear;
+close all;
+format long
+% falseposition
+ f = @(x) (x-cos(x));
+ a = 0 ;
+ b = 1 ;
+ x = ((a * f(b))-(b * f(a))) / (f(b) - f(a));
+
+
+i = 1 ;
+for c=1:6
+     x = ((a * f(b))-(b * f(a))) / (f(b) - f(a));
+    % if x&a<0
+    if f(a)*f(x) < 0
+        b = x ;
+    % if x&b<0    
+    else 
+        a = x ;   
+    end
+
+    % answer table 
+    STR = ['n = ', num2str(i), ', x = ' ,num2str(x), ', f(x) = ' ...
+    ,num2str(f(x))];
+    disp(STR)
+    i=i+1;
+
+   
+end
